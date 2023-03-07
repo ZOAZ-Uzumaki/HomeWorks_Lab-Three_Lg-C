9ce924643a44a14a24b530259e0340a2ad53034d
Tymur <105642945+ZOAZ-Uzumaki@users.noreply.github.com>
2023-03-07 17:30:38 +0100

    Завдання виконано.
    Завдання виконано.

---
+++ HomeWorksLabThree/C#/HWLT-For21/Prodject/HMLT_DZProdject/HMLT_DZProdject/Program.cs
@@ -0,0 +1,27 @@
+﻿//For21.
+
+/* Дано целое число N (> 0). Используя один цикл, найти сумму
+1 + 1 / (1!) + 1 / (2!) + 1 / (3!) + . . . + 1 / (N!)
+Цикл с параметром 27
+(выражение N! — N–факториал — обозначает произведение всех целых
+чисел от 1 до N: N! = 1·2·. . .·N). Полученное число является приближенным значением константы e = exp(1).
+*/
+
+Console.OutputEncoding = System.Text.Encoding.Default; //lg Ukraine
+
+Console.WriteLine("Задайте ціле число - " + "N" );
+int N = Convert.ToInt16( Console.ReadLine() ); //водим N;
+
+double factorial = 1.0;
+double suma = 1.0;
+
+for( int i = 1; i <= N; i++)
+{
+    factorial*= i; // Обчислюємо факторіал.
+
+    double slogaemoe = 1.0 / factorial;  suma += slogaemoe;
+}
+
+Console.WriteLine("Результат- "+ suma );
+
+Console.ReadKey();
---
+++ HomeWorksLabThree/C++/HWLT-While 21/Prodject/CPP_DUBLICATE_Charp/CPP_DUBLICATE_Charp/CPP_DUBLICATE_Charp.cpp
@@ -0,0 +1,32 @@
+﻿/*. Дано целое число N (> 0). С помощью операций деления нацело
+и взятия остатка от деления определить, имеются ли в записи числа N
+нечетные цифры. Если имеются, то вывести TRUE, если нет — вывести
+FALSE.
+*/
+#include <iostream>;
+#include <Windows.h>;
+using namespace std;
+
+int main(void)
+{
+    SetConsoleCP(1251);           //lg Ukraine
+    SetConsoleOutputCP(1251);     //lg Ukraine
+
+    cout << "Будь ласка ведіть значення  " << "N" << endl;
+    int N; cin >> N; bool StopWhileRestarted = false;
+    
+    while (N > 0)
+    {
+        int digit = N % 10;
+        if (digit %2 != 0)
+        {
+            cout << "TRUE" << endl;
+            StopWhileRestarted = true; break;  
+        }
+        else
+        {
+            cout << "FALSE" << endl;
+            StopWhileRestarted = true; break;
+        }
+    }
+}
